33c11ba5e9c0d597b45145a6eb31f40a0a537360
sachinmandloi1921 <mandloisachin08@gmail.com>
2023-08-18 13:16:52 +0530

    Done

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Sachin Mandloi
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
